783c90d7236453e5a68f26292ca5903871e3ddd2
tangjunxiang0905 <1976259749@qq.com>
2018-09-29 20:25:29 +0800

    分类管理 - 一级分类添加完成

--- public/back/js/first.js
+++ public/back/js/first.js
@@ -18,8 +18,8 @@ $(function () {
                 console.log(info);
                 var htmlStr = template('tmp', info);
                 $('tbody').html(htmlStr);
-    
-                    // 分页初始化
+
+                // 分页初始化
                 $('#paginator').bootstrapPaginator({
                     // 规定版本号必须为3
                     bootstrapMajorVersion: 3,
@@ -44,8 +44,58 @@ $(function () {
 
 
     // 2.点击添加分类按钮  显示添加模态框
-    $('#addBtn').on('click',function() {
+    $('#addBtn').on('click', function () {
         $('#addModal').modal('show');
     })
+
+
+    // 3-通过表单校验插件   实现表单校验功能
+    $('#form').bootstrapValidator({
+        //配置图标
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',   //校验成功
+            invalid: 'glyphicon glyphicon-remove',  //校验失败
+            validating: 'glyphicon glyphicon-refresh'  //校验中
+        },
+        // 配置校验字段
+        fields: {
+            categoryName: {
+                //配置校验字段
+                validators: {
+                    notEmpty: {
+                        message: '请输入一级分类名称'
+                    }
+                }
+            }
+        }
+    })
+
+    // 4-注册表单校验成功事件 阻止校验成功时默认提交    通过ajax提交
+    $('#form').on('success.form.bv', function (e) {
+        e.preventDefault();
+
+
+        // 通过ajax提交
+        $.ajax({
+            url: '/category/addTopCategory',
+            type: 'post',
+            data: $('#form').serialize(),
+            dataType: 'json',
+            success: function (info) {
+                // console.log(info);
+                if (info.success) {
+                    // 关闭模态框
+                    $('#addModal').modal('hide');
+                    // 重新渲染页面
+                    currentPage = 1;
+                    render();
+                    // 表单内容和校验状态都要重置   resetForm   默认参数是false   只有传true 才会将内容和状态都重置
+                    $('#form').data('bootstrapValidator').resetForm(true);//重置表单，并且会隐藏所有的错误提示和图标
+                }
+            }
+        })
+    })
+
+
 
 })
